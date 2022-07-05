9c1ef159e1d6247642d4f9bf2af22bbce67dae5e
agiorgio99 <agiorgio99@gmail.com>
2022-07-05 19:42:50 +0200

    aggiustati bottoni iniziali

---
+++ js/buttons.js
@@ -0,0 +1,10 @@
+function hide_start_button(){
+    document.getElementById(1).style.display=' none';
+    document.getElementById(1).style.visibility="hidden";
+    document.getElementById(2).style.display=' inline';
+    document.getElementById(2).style.visibility="visible";
+    document.getElementById(3).style.display=' inline';
+    document.getElementById(3).style.visibility="visible";
+    document.getElementById(4).style.display=' inline';
+    document.getElementById(4).style.visibility="visible";
+}
