0a6b2f8908d3ed5f639cd1484ea4d99d4850012e
ACec99 <c.99.cecilia@gmail.com>
2022-07-05 22:29:57 +0200

    insertion of the plane and initial castle byers

--- js/mappa.js
+++ js/mappa.js
@@ -1,19 +1,61 @@
 import * as THREE from 'https://cdn.skypack.dev/three@v0.129.0-oVPEZFilCYUpzWgJBZqM/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@v0.129.0-oVPEZFilCYUpzWgJBZqM/examples/jsm/loaders/GLTFLoader.js';
 
-let scene = new THREE.Scene();
+var scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
-const fov = 60;
+const fov = 75;
 const aspect =  window.innerWidth / window.innerHeight;
 const near = 1.0;
 const far = 1000.0;
 let camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 var renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight); //render resolution
 document.body.appendChild(renderer.domElement);
+
+const textureLoader = new THREE.TextureLoader();
+/*const tilesBaseColor = textureLoader.load('../texture_grass2/Hedge_001_BaseColor.jpg');
+const tilesNormalMap = textureLoader.load('../texture_grass2/Hedge_001_Normal.jpg');
+const tilesHeightMap = textureLoader.load('../texture_grass2/Hedge_001_Height.png');
+const tilesRoughness = textureLoader.load('../texture_grass2/Hedge_001_Roughness.jpg');
+const tilesAmbientOcclusionMap = textureLoader.load('../texture_grass2/Hedge_001_AmbientOcclusion.jpg');
+const tilesMaterial = textureLoader.load('../texture_grass2/Material_2044.jpg');*/
+
+const tilesBaseColor = textureLoader.load('../texture_grass2/Ground037_1K_Color.jpg');
+const tilesNormalMap = textureLoader.load('../texture_grass2/Ground037_1K_NormalDX.jpg');
+const tilesHeightMap = textureLoader.load('../texture_grass2/Ground037_1K_Displacement.jpg');
+const tilesRoughness = textureLoader.load('../texture_grass2/Ground037_1K_HRoughness.jpg');
+const tilesAmbientOcclusionMap = textureLoader.load('../texture_grass2/Ground037_1K_AmbientOcclusion.jpg');
+
+
+//CREATION OF THE PLANE (GRASS)
+var geometry = new THREE.BoxGeometry(8,0.1,7);
+var material = new THREE.MeshBasicMaterial(
+    {
+        //BUMP MAPPING
+        map: tilesBaseColor,
+        normalMap: tilesNormalMap,
+        displacementMap: tilesHeightMap,
+        roughnessMap: tilesRoughness, //define which areas are rough
+        roughness: 0.5,
+        aoMap: tilesAmbientOcclusionMap //put the shadow areas
+    }
+);
+var plane = new THREE.Mesh(geometry,material);
+
+plane.position.z = -5;
+plane.geometry.attributes.uv2 = plane.geometry.attributes.uv; //this because the aoMap requires a second set of UVs 
+                                                            // => Uvs are used to map the texture to a point of the geometry
+plane.rotation.x = 10;
+plane.rotation.y = 0.0;
+
+scene.add(plane);
+
+
+renderer.render(scene,camera); 
+
 const loader = new GLTFLoader();
 var obj;
-loader.load('../models/map/scene.gltf', function(gltf){
+loader.load('../models/map/castle_byers/scene.gltf', function(gltf){
     obj = gltf.scene;
     scene.add(gltf.scene);
     //render.render(scene,camera);
@@ -21,25 +63,11 @@ loader.load('../models/map/scene.gltf', function(gltf){
 }, undefined, function ( error ) {
     console.error( error );
 } );
-let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-/*light.position.set(20, 100, 10);
-light.target.position.set(0, 0, 0);
-light.castShadow = true;
-light.shadow.bias = -0.001;
-light.shadow.mapSize.width = 2048;
-light.shadow.mapSize.height = 2048;
-light.shadow.camera.near = 0.1;
-light.shadow.camera.far = 500.0;
-light.shadow.camera.near = 0.5;
-light.shadow.camera.far = 500.0;
-light.shadow.camera.left = 100;
-light.shadow.camera.right = -100;
-light.shadow.camera.top = 100;
-light.shadow.camera.bottom = -100;*/
+var light = new THREE.HemisphereLight(0xffffff, 0x000000, 9);
 scene.add(light);
 
-//let light = new THREE.AmbientLight(0x101010);
-//scene.add(light);
+/*let light = new THREE.AmbientLight(0x101010);
+scene.add(light);*/
 camera.position.set(0,1,2);
 function animate() {
     requestAnimationFrame(animate);
