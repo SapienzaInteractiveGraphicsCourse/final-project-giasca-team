98ecb27f3c58899e71957ad1e2f8d69d26f5625a
agiorgio99 <agiorgio99@gmail.com>
2022-07-08 19:53:09 +0200

    aggiunto audio start

--- js/buttons.js
+++ js/buttons.js
@@ -1,4 +1,4 @@
-function hide_start_button(){
+function start_button(){
     document.getElementById(1).style.display=' none';
     document.getElementById(1).style.visibility="hidden";
     document.getElementById(2).style.display=' inline';
@@ -10,4 +10,7 @@ function hide_start_button(){
     document.getElementById(5).style.display=' inline';
     document.getElementById(5).style.visibility="visible";
 
+    var start_audio= new Audio('../Stranger_Things.mp3');
+    start_audio.loop=true;
+    start_audio.play();
 }
