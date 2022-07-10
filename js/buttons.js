c6611f8587e39c629d081f00e42dfe6ba4eb6a03
agiorgio99 <agiorgio99@gmail.com>
2022-07-10 11:30:22 +0200

    implementati popup bottoni

--- js/buttons.js
+++ js/buttons.js
@@ -14,3 +14,24 @@ function start_button(){
     start_audio.loop=true;
     start_audio.play();
 }
+
+const popupBox = document.querySelector(".popup-box");
+const closeBtn = document.querySelector(".close-btn");
+
+function instruction_button(){
+    document.querySelector("#instruction-popup").classList.add("active");
+}
+
+function difficulty_button(){
+    document.querySelector("#difficulty-popup").classList.add("active");
+}
+
+function options_button(){
+    document.querySelector("#options-popup").classList.add("active");
+}
+
+function close_button(){
+    document.querySelector("#instruction-popup").classList.remove("active"); //Close the popup screen on click the close button.
+    document.querySelector("#difficulty-popup").classList.remove("active");
+    document.querySelector("#options-popup").classList.remove("active");
+}
